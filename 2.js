b3294149c61380e7c8e7c4c632a181629a2a2dc5
gjubelit-source <gjubelit@gmail.com>
2025-08-02 11:44:55 -0500

    subiendo archivo

---
+++ 2.js
@@ -0,0 +1,10 @@
+let numero=30;
+console.log("El valor del numero:"+numero);
+let nombre="jubelit zapata";
+let edad=17;
+let sueldo=15000000;
+let hijos=0 ;
+console.log("Hola soy " +nombre +" \ntengo "+ edad +" \nAños "+"\nquiero un sueldo de: "+sueldo +" \ne hijos "+hijos);
+let femenino=true;
+let masculino=false
+console.log("Mi género es "+" femenino+"+ femenino);
